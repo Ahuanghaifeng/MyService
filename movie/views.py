6f2ab50f60178afb244defbcbe8b451c977a3fc5
Ahuanghaifeng <1490738477@qq.com>
2017-11-13 10:51:47 +0800

    添加友盟统计

--- movie/views.py
+++ movie/views.py
@@ -15,10 +15,61 @@ sys.setdefaultencoding('utf-8')
 def movie_index(request):
     from movie.pager import Pagination
     # pk 是Primary key主键的意思即为文章id
-    movie = models.china.objects.all()
+    movie = models.newmovie.objects.all()
     page = request.GET.get('p')  #接收网页中的page值
     page_obj = Pagination(len(movie), page)
     print page
     print page_obj.start()
     movies = movie[page_obj.start():page_obj.end()]
     return render(request, 'movie/index.html', {'movies': movies,'page_obj':page_obj})
+
+def movie_china(request):
+    from movie.pager import Pagination
+    # pk 是Primary key主键的意思即为文章id
+    movie = models.china.objects.all()
+    page = request.GET.get('p')  # 接收网页中的page值
+    page_obj = Pagination(len(movie), page)
+    print page
+    print page_obj.start()
+    movies = movie[page_obj.start():page_obj.end()]
+    return render(request, 'movie/index.html', {'movies': movies, 'page_obj': page_obj})
+
+def movie_japan(request):
+    from movie.pager import Pagination
+    # pk 是Primary key主键的意思即为文章id
+    movie = models.japanmovie.objects.all()
+    page = request.GET.get('p')  # 接收网页中的page值
+    page_obj = Pagination(len(movie), page)
+    print page
+    print page_obj.start()
+    movies = movie[page_obj.start():page_obj.end()]
+    return render(request, 'movie/index.html', {'movies': movies, 'page_obj': page_obj})
+
+def movie_america(request):
+    from movie.pager import Pagination
+    # pk 是Primary key主键的意思即为文章id
+    movie = models.americamovie.objects.all()
+    page = request.GET.get('p')  # 接收网页中的page值
+    page_obj = Pagination(len(movie), page)
+    print page
+    print page_obj.start()
+    movies = movie[page_obj.start():page_obj.end()]
+    return render(request, 'movie/index.html', {'movies': movies, 'page_obj': page_obj})
+
+def movie_detail(request):
+    movie_id = request.GET.get('id')
+    movie = models.china.objects.get(pk=movie_id)
+    img = models.china.objects.filter(pk=movie_id).values("movie_image")
+    imgs = list(img)
+    dictimg = imgs[0]
+    imageurl = dictimg['movie_image']
+    print imageurl
+    if ',' in imageurl:
+        image1 = imageurl.split(',')[0]
+        image2 = imageurl.split(',')[1]
+        return render(request, "movie/detail.html", {'movie': movie,'image1':image1,'image2':image2})
+    else:
+        image1 = imageurl
+        return render(request, "movie/detail.html", {'movie': movie,'image1':image1})
+
+
